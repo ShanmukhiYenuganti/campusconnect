060bee6eabb0d9ec5c5380ae3f03759708967aa2
Shanmukhi Yenuganti <shanmukhiyenuganti@gmail.com>
2026-04-19 20:50:34 +0530

    fix root route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("Backend is LIVE 🚀")
+});
+
 // Routes
 app.use("/api/events", require("./routes/events"))
 app.use("/api/registrations", require("./routes/registrations"))
@@ -33,3 +37,7 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+app.use("*", (req, res) => {
+  res.status(404).send("Route not found ❌");
+});
